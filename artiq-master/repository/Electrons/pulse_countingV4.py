b1b6dc7e63a15b2fe7151f1ed66702d5ac769002
hemmerlinglab <hemmerlinglab@gmail.com>
2021-08-18 17:06:43 -0700

    removed V5 file

--- artiq-master/repository/Electrons/pulse_countingV4.py
+++ artiq-master/repository/Electrons/pulse_countingV4.py
@@ -18,55 +18,37 @@ def print_underflow():
     print('RTIO underflow occured')
 
 
-#def pc(self,counts): 
-#    print(counts)
-
 # Class which defines the pmt counting experiment
 class pulse_counting4(EnvExperiment):
     def build(self):
          self.setattr_device('core') # need the core for everything
          self.setattr_device('ttl3') # where pulses are being sent in by ttl
-         # set arguments that can be varied on the dashboard 
-         
-         self.setattr_argument('step_size',NumberValue(default=100,unit='ms',scale=1,ndecimals=0,step=1)) #time scale on the scope,how zoomed you are in time
-    
          self.setattr_argument('time_count', NumberValue(default=400,unit='number of counts',scale=1,ndecimals=0,step=1)) #how many indices you have in time axis
          self.setattr_argument('detection_time',NumberValue(default=100,unit='ms',scale=1,ndecimals=0,step=1))
          self.setattr_device('scheduler') # scheduler used
     def prepare(self):
-	# this function runs before the experiment, set dataset variables here
-        self.time_interval=np.linspace(0,(self.step_size)*(self.time_count-1)/1.0e3,self.time_count)
-        self.set_dataset('times',(self.time_interval),broadcast=True)
         self.set_dataset('count_tot',[0]*self.time_count,broadcast=True)
     def run(self):
         self.core.reset()
+        # while loop continuously repopulates the graph
         while True:
             self.scheduler.pause() # allows for "terminate instances" functionality
-         #   delay(100*ms)
-            self.run_pmt()
-        #    delay(100*ms)
+            self.counting()
    
-    # run_pmt, this is directly counting pulses in FPGA and decorated with kernel so that artiq is listening/waiting for a pulse for 100ms        
     @kernel
-    def run_pmt(self):
+    def counting(self):
         self.core.break_realtime()
-        #self.ttl3.init() #initializes sampler
 
-        # read the counts and store into a dataset
-        
-        # single step in time, defines the length of the list as the time count
-        
-        # save the number of counts into a variable called data0
+        # read the counts and store into a dataset for live updating
         for j in range(self.time_count):
             #register rising edges for detection time
-            t_count= self.ttl3.gate_rising(self.detection_time*ms) # reads from the channel
+            t_count= self.ttl3.gate_rising(500*ms) # reads from the channel
             count =self.ttl3.count(t_count)
             # mutate dataset at index j with the value of count
-            self.mutate_dataset('count_tot',j,count)
+            self.mutate_dataset('count_tot',j,count/500.0e-3)
             # delay for as long your listening for, translates between machine time and actual time
-            delay(self.detection_time*ms)
+            delay(500*ms)
         
-        #self.set_dataset('TTL_counts',(count_tot),broadcast=True)
         
 
 
