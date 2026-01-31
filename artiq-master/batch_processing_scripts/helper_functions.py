9f348e7f20573d0194891a48f9fc45ffacee174a
hemmerlinglab <hemmerlinglab@gmail.com>
2026-01-30 17:10:50 -0800

    sync

--- artiq-master/batch_processing_scripts/helper_functions.py
+++ artiq-master/batch_processing_scripts/helper_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import find_peaks
 
 def load_data(timestamp, ynames = ["ratio_signal"]):
     date, _ = timestamp.split("_")
@@ -33,4 +34,56 @@ def dummy_fine_scan_range():
 def dummy_optima():
     return -0.13
 
+def find_peaks_and_plot(x, y, noise):
+
+    peaks = find_peaks(y, height=6*noise, prominence=3*noise)
+
+    plt.plot(x, y)
+    plt.axhline(y=6*noise, linestyle="--")
+    for pk in peaks[0]:
+        plt.axvline(x=x[pk], linestyle="--")
+    plt.show()
+
+    return peaks
+
+def construct_scan_ranges(y, peaks):
+
+    for i, pk in enumerate(peaks[0]):
+        h_left = y[pk] - y[left_bases[i]]
+        h_right = y[pk] - y[right_bases[i]]
+
 def analyze_rough_scan(timestamp):
+
+    # Extract Data
+    x, ys = load_data(timestamp, ynames = ["ratio_signal", "ratio_lost"])
+    y1 = -ys["ratio_signal"]
+    y2 = ys["ratio_lost"]
+
+    # Normalize Data
+    baseline1 = np.sort(y1)[:int(len(y1)*0.25)].mean()
+    baseline2 = np.sort(y2)[:int(len(y2)*0.25)].mean()
+    peak1 = np.max(y1)
+    peak2 = np.max(y2)
+    y_trapped = (y1 - baseline1) / (peak1 - baseline1)
+    y_lost = (y2 - baseline2) / (peak2 - baseline2)
+
+    # Noise Level
+    noise_trapped = np.sort(y_trapped)[:int(len(y_trapped)*0.25)].std()
+    noise_lost = np.sort(y_lost)[:int(len(y_lost)*0.25)].std()
+
+    # Peak Search
+    peaks_trapped = find_peaks(
+        y_trapped,
+        height=max(6*noise_trapped, 0.15),
+        prominence=max(3*noise_trapped, 0.10),
+        distance=5
+    )
+    peaks_lost = find_peaks(
+        y_lost,
+        height=max(6*noise_lost, 0.15),
+        prominence=max(3*noise_lost, 0.10),
+        distance=5
+    )
+
+    # Construct Fine Scan Ranges
+    for pk in peaks_trapped
